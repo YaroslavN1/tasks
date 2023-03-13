0e2be1b00b68573480fce6e67a6701d26c2e2a54
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2023-03-13 16:41:21 +0100

    Made task data visible on a tasks list.

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,6 +1,40 @@
 <template>
   <v-card>
-    {{ props.task.id }} {{ props.task.name }}
+    <v-list class="d-flex flex-row">
+      <v-list-item>
+        <v-chip
+          label
+        >
+          {{ props.task.category }}
+        </v-chip>
+      </v-list-item>
+
+      <v-list-item>
+        <p>
+          {{ props.task.name }}
+        </p>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list
+          class="d-flex flex-row py-0"
+          lines="one"
+        >
+          <v-list-item
+            v-for="item in taskEntries"
+            :key="item[0]"
+          >
+            <p>
+              {{ item[0] }}
+            </p>
+
+            <p>
+              {{ item[1] }}
+            </p>
+          </v-list-item>
+        </v-list>
+      </v-list-item>
+    </v-list>
   </v-card>
 </template>
 
@@ -14,5 +48,12 @@
     task: Object,
     required: true
   })
+
+/*
+  task object to entries
+*/
+
+  const taskEntries = Object.entries(props.task).slice(3)
+  taskEntries.map((entry) => entry[0] = entry[0].replace(/_/g, " "))
 
 </script>
